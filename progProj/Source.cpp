2e4ce1306e2068aa2dc9ce3cf943b0052a9fa2f6
1501029 <1501029@eng.asu.edu.eg>
2017-05-06 19:54:17 +0200

    Finished Level one :D

--- progProj/Source.cpp
+++ progProj/Source.cpp
@@ -62,20 +62,7 @@ int main() {
 			cout << "none" << endl;
 //****************************************************************************************************End of 1,2 Operations
 
-//Cutting Polygons in arrays
-		string pol[1][1000];  // pol[1][polygon's number]
 
-		int firstSemi = polygons.find(";");
-		int secSemi = polygons.find(";", firstSemi + 1);
-		for (int i = 0; i <= 2; i++) {
-			if (firstSemi != string::npos) {
-				pol[0][i] = polygons.substr(firstSemi + 1, secSemi - firstSemi - 1);
-				firstSemi = secSemi;
-				secSemi = polygons.find(";", firstSemi + 1);
-			}
-			else
-				break;
-		}
 //finding X and Y of all points and storing them in two arrays
 	//Changning Format
 		string str = polygons;
@@ -99,8 +86,8 @@ int main() {
 		}
 		
 	//Array of X points
-		float xPoints[10000] = {0};
-		float yPoints[10000] = {0};
+		float xPoints[10000] ;
+		float yPoints[10000] ;
 		int firstS = str.find("s");
 		int secS = str.find("s", firstS + 1);
 		for (int i = 0; i <= str.length(); i++) {
@@ -132,48 +119,108 @@ int main() {
 		float Xmax = xPoints[0];
 		float Xmin = xPoints[0];
 		for (int i = 0; i < 10000; i++) {
-			if (xPoints[i] > Xmax)
-				Xmax = xPoints[i];
-			if (xPoints[i] < Xmin)
-				Xmin = xPoints[i];
+			if (xPoints[i] != -107374176.) {
+				if (xPoints[i] > Xmax)
+					Xmax = xPoints[i];
+				if (xPoints[i] < Xmin)
+					Xmin = xPoints[i];
+			}
 		}
-		if (Xmin != 0)
-			cout << Xmin << endl;	//points number
-		else
-			cout << "none" << endl;
-		if (Xmax != 0)
-			cout << Xmax << endl;	//points number
-		else
-			cout << "none" << endl;
+
+			cout << Xmin << endl;	//Min X
+
+			cout << Xmax << endl;	//Max X
+
 
 
 //Finding and printing Max Y and min Y
 		float Ymax = yPoints[0];
 		float Ymin = yPoints[0];
 		for (int i = 0; i < 10000; i++) {
-			if (yPoints[i] > Ymax)
-				Ymax = yPoints[i];
-			if (yPoints[i] < Ymin)
-				Ymin = yPoints[i];
+			if (yPoints[i] != -107374176.) {
+				if (yPoints[i] > Ymax)
+					Ymax = yPoints[i];
+				if (yPoints[i] < Ymin)
+					Ymin = yPoints[i];
+			}
 		}
-		if (Ymin != 0)
-			cout << Ymin << endl;	//points number
-		else
-			cout << "none" << endl;
-		if (Ymax != 0)
-			cout << Ymax << endl;	//points number
-		else
-			cout << "none" << endl;
+		
+			cout << Ymin << endl;	//Min Y
+		
+			cout << Ymax << endl;	//Max Y
+		
 
-//*****************************************************************************************End of 3,4,5,6 Operations
+//Printing The enclousing rectanngle
+		cout << "(" << Xmax << "," << Ymax << ")" << "," 
+		 	 << "(" << Xmax << "," << Ymin << ")" << "," 
+			 << "(" << Xmin << "," << Ymin << ")" << "," 
+			 << "(" << Xmin << "," << Ymax << ")"  << endl;
+
+//*****************************************************************************************End of 3,4,5,6,7 Operations
+
+//Cutting Polygons in arrays
+		string pol[1000];  // pol[polygon's number-1] array of polygons
+
+		int firstSemi = polygons.find(";");
+		int secSemi = polygons.find(";", firstSemi + 1);
+		for (int i = 0; i <= 100; i++) {
+			if (firstSemi != string::npos) {
+				pol[i] = "s" + polygons.substr(firstSemi + 1, secSemi - firstSemi - 1) + "s";
+				firstSemi = secSemi;
+				secSemi = polygons.find(";", firstSemi + 1);
+			}
+			else
+				break;
+		}
+		
+
+//finding Redundant points (repeated points) (point lying on a line of previous and next points)
+		// repeated points
+
+		//removing separiting commas and putting s instead of it
+		for (int j = 0; j < 1000; j++) {
+			for ( int i = 0; i < pol[j].length(); i++){
+				if (pol[j].substr(i, 3) == "),(")
+					pol[j].replace(i, 3, ")s(");
+			}
+		}
+		string polygonsPoints[100][100];//array of 100 polygons and 100 points for each poylgon
+		for (int j = 0; j < 100; j++) {
+			int s1 = pol[j].find("s");
+			int s2 = pol[j].find("s", s1 + 1);
+			int repeat = 0;
+			for (int i = 0; i <= pol[j].length(); i++) {
+				if (pol[j].substr(i, 1) == "s")
+					repeat++;
+			}
+			for (int i = 0; i <= repeat - 1; i++) {
+				if (s1 != string::npos) {
+					polygonsPoints[j][i] = pol[j].substr(s1 + 1, s2 - s1 - 1);
+					s1 = s2;
+					s2 = pol[j].find("s", s1 + 1);
+				}
+				else
+					break;
+			}
+		}
+		//counting reduntant points 
+		int redNo = 0;
+		for (int j = 0; j < 100; j++) {
+			for (int i = 0; i < 100; i++) {
+				if (polygonsPoints[j][i] != "") {
+					if (polygonsPoints[j][i] == polygonsPoints[j][i + 1]) {
+						redNo++;
+					}
+				}
+			}
+		}
+		cout << redNo << endl;
+		// point lying on a line of previous and next points
 
 
 
 
-
-
-
-	}
+	} // end of last(9) operation of level 1
 	return 0;
 }
 
